60fb24e32ffb43db1b1cd915caef917dd8dc52b0
dearLiuch <1131962184@qq.com>
2021-02-24 21:09:22 +0800

    first commit

---
+++ src/test/java/com/lch/study/mybatisstudy/MybatisstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lch.study.mybatisstudy;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
